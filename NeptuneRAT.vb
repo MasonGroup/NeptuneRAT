3f5ba77816dff6206c8f652766a05ce7300a95be
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2024-12-10 16:12:48 +0300

    Update NeptuneRAT.vb

--- NeptuneRAT.vb
+++ NeptuneRAT.vb
@@ -2,7 +2,7 @@ Imports System.Windows.Forms
 
 Module Program
     Sub Main()
-        MessageBox.Show("Gangsta freemasonry made me do it", "SVCINJ")
+        MessageBox.Show("Gangsta freemasonry made me do it", "NeptuneRAT")
         Environment.Exit(0)
     End Sub
 End Module
