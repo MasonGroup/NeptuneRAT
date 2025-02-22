afbc6aeccdd5bd78e4269335c33d5f66c7a91723
Freemasonry <95870255+MasonGroup@users.noreply.github.com>
2025-02-22 20:44:33 +0300

    Create NeptuneRat.vb

---
+++ NeptuneRat.vb
@@ -0,0 +1,8 @@
+Imports System.Windows.Forms
+
+Module Program
+    Sub Main()
+        MessageBox.Show("Gangsta freemasonry made me do it", "NeptuneRat")
+        Environment.Exit(0)
+    End Sub
+End Module
